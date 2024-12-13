0eaa803c87eb4ac2c71f32d8b93a3cc3fe6fe6a8
mucel29 <nroboguy@gmail.com>
2024-12-13 20:36:37 +0200

    Laborator 4 & 5

--- 2-foundations-of-rust/3-advanced-syntax/5-boxed-data/src/main.rs
+++ 2-foundations-of-rust/3-advanced-syntax/5-boxed-data/src/main.rs
@@ -6,7 +6,7 @@
 /// - Add support for multiplication and division
 ///
 /// - We have added the form "Summation(Vec<Expr>)", representing the sum of a list of expressions.
-/// Question: why can we get away with Vec<Expr> enough in that case, instead of Box<Vec<Expr>> ?
+///     Question: why can we get away with Vec<Expr> enough in that case, instead of Box<Vec<Expr>> ?
 ///
 /// - EXTRA: Since division can fail, the function eval needs to return an Option<i64>, where None indicates that a division by
 ///   zero has occurred. Can you change the code so that that errors are propagated correctly? (hint: use the ? syntax).
@@ -16,8 +16,11 @@ enum Expr {
     Const(i64),
     Add(Box<Expr>, Box<Expr>),
     Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
     Var,
     Summation(Vec<Expr>),
+    Sigma(i64, i64, Box<Expr>),
 }
 
 // inject these two identifiers directly into the current namespace
@@ -36,30 +39,52 @@ fn sub(x: Expr, y: Expr) -> Expr {
 }
 
 fn mul(x: Expr, y: Expr) -> Expr {
-    todo!()
+    Expr::Mul(Box::new(x), Box::new(y))
 }
 
 fn div(x: Expr, y: Expr) -> Expr {
-    todo!()
+    Expr::Div(Box::new(x), Box::new(y))
+}
+
+fn sigma(x: i64, y: i64, z: Expr) -> Expr {
+    Expr::Sigma(x, y, Box::new(z))
 }
 
 // ...
 
-fn eval(expr: &Expr, var: i64) -> i64 {
+fn eval(expr: &Expr, var: i64) -> Option<i64> {
     // this should return an Option<i64>
     use Expr::*;
     match expr {
-        Const(k) => *k,
-        Var => var,
-        Add(lhs, rhs) => eval(lhs, var) + eval(rhs, var),
-        Sub(lhs, rhs) => eval(lhs, var) - eval(rhs, var),
+        Const(k) => Some(*k),
+        Var => Some(var),
+        Add(lhs, rhs) => Some(eval(lhs, var).unwrap() + eval(rhs, var).unwrap()),
+        Sub(lhs, rhs) => Some(eval(lhs, var).unwrap() - eval(rhs, var).unwrap()),
+
+        Mul(lhs, rhs) => Some(eval(lhs, var).unwrap() * eval(rhs, var).unwrap()),
+        Div(lhs, rhs) => {
+            if eval(rhs, var).unwrap() != 0 {
+                Some(eval(lhs, var).unwrap() / eval(rhs, var).unwrap())
+            } else {
+                None
+            }
+        }
 
         Summation(exprs) => {
             let mut acc = 0;
             for e in exprs {
-                acc += eval(e, var);
+                acc += eval(e, var).unwrap();
             }
-            acc
+            Some(acc)
+        }
+
+        Sigma(from, to, expr) => {
+            let mut acc = 0;
+            for i in *from..=*to {
+                acc += eval(expr, i).unwrap();
+            }
+
+            Some(acc)
         }
     }
 }
@@ -71,7 +96,7 @@ fn main() {
             "{:?} with Var = {} ==> {}",
             &expr,
             value,
-            eval(&expr, value)
+            eval(&expr, value).unwrap()
         );
     };
 
@@ -81,6 +106,16 @@ fn main() {
     test(sub(Var, Var));
     test(add(sub(Var, Const(5)), Const(5)));
     test(Summation(vec![Var, Const(1)]));
+    test(mul(Const(5), Const(7)));
+    test(div(Const(16), Const(4)));
+    //test(div(Var, Const(0)));
+
+    //15
+    test(sigma(1, 5, Var));
+    // 30
+    test(sigma(1, 5, mul(Const(2), Var)));
+    // (5 * 6 * 11) / 6
+    test(sigma(1, 5, mul(Var, Var)));
 }
 
 #[cfg(test)]
@@ -90,12 +125,12 @@ mod test {
     #[test]
     fn test_cases() {
         let x = 42;
-        assert_eq!(eval(&Const(5), x), 5);
-        assert_eq!(eval(&Var, x), 42);
-        assert_eq!(eval(&sub(Var, Const(5)), x), 37);
-        assert_eq!(eval(&sub(Var, Var), x), 0);
-        assert_eq!(eval(&add(sub(Var, Const(5)), Const(5)), x), 42);
-        assert_eq!(eval(&Summation(vec![Var, Const(1)]), x), 43);
+        assert_eq!(eval(&Const(5), x).unwrap(), 5);
+        assert_eq!(eval(&Var, x).unwrap(), 42);
+        assert_eq!(eval(&sub(Var, Const(5)), x).unwrap(), 37);
+        assert_eq!(eval(&sub(Var, Var), x).unwrap(), 0);
+        assert_eq!(eval(&add(sub(Var, Const(5)), Const(5)), x).unwrap(), 42);
+        assert_eq!(eval(&Summation(vec![Var, Const(1)]), x).unwrap(), 43);
     }
 }
 
